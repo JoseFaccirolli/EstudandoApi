27bdcf22a30c624d76888599edd5ceb502c41c2b
JoseFaccirolli <jose.faccirolli08@gmail.com>
2025-12-24 01:57:09 -0300

    Reversao: API funcionando de volta | refatiracao sera feita em uma nova branch

--- src/database/testConnect.js
+++ src/database/testConnect.js
@@ -1,10 +1,15 @@
 const connect = require("./connect");
 
-module.exports = async function testConnect(){
+module.exports = function testConnect(){
     try {
         const query = `SELECT "Sucessfull connection" AS Mensagem`;
-        const [result] = await connect.execute(query);
-        console.log(result[0].Mensagem);
+        connect.query(query, function(err){
+            if(err){
+                console.log("Conection not established. " + err);
+                return;
+            }
+            console.log("Conection established with MySQL.");
+        })
     } catch (error) {
         console.error("Error executing SQL query. ", error);
     }
